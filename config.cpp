8f976704fb4fed0de624c56ab74d8bb9859a6872
Gavin Gniewek <GavinMichaelGniewek+Github@gmail.com>
2026-02-04 22:56:12 -0500

    updated folder structure
    updates

--- config.cpp
+++ config.cpp
@@ -1,2 +1,13 @@
-#include "CfgPatches.hpp"
-#include "autogen.hpp"
+class CfGPatches {
+	class Lazy_Custom_Factions_Mod {
+		name = "Lazy Custom Factions Mod Master";
+		author = "Lazy";
+		units[] = {
+		};
+		requiredAddons[] = {
+		};
+		weapons[] = {    
+	    };
+		skipWhenMissingDependencies = 1;		
+	};
+};
